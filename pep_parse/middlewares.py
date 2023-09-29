b963e58e7bc0d5d945cc78b0f1ba37afe0f1498a
Евгений Каликов <eskalikov@yandex.com>
2023-09-29 18:15:52 +0500

    Review 1. Fix README & meddlewares

--- pep_parse/middlewares.py
+++ pep_parse/middlewares.py
@@ -13,15 +13,15 @@ class PepParseSpiderMiddleware:
         return None
 
     def process_spider_output(self, response, result, spider):
-        for i in result:
-            yield i
+        for item in result:
+            yield item
 
     def process_spider_exception(self, response, exception, spider):
         pass
 
     def process_start_requests(self, start_requests, spider):
-        for r in start_requests:
-            yield r
+        for request in start_requests:
+            yield request
 
     def spider_opened(self, spider):
         spider.logger.info('Spider opened: %s' % spider.name)
